0964e29c3e48420fa8a3c97224229491e250de2e
Axel Petersson <axel.818@gmail.com>
2024-03-28 10:51:59 +0100

    adds grid layout to app.tsx

--- KindCoders-Wordle/src/App.tsx
+++ KindCoders-Wordle/src/App.tsx
@@ -1,9 +1,48 @@
 function App() {
 
   return (
-    <>
+    
+    <section className="container">
+      
       <h1>Hello World</h1>
-    </>
+      
+      <input type="text" name="" id="" />
+      <ul className="grid">
+      <li>h</li>
+      <li>e</li>
+      <li>j</li>
+      <li>d</li>
+      <li>å</li>
+      </ul>
+      <ul className="grid">
+      <li>h</li>
+      <li>e</li>
+      <li>j</li>
+      <li>d</li>
+      <li>å</li>
+      </ul>
+      <ul className="grid">
+      <li>h</li>
+      <li>e</li>
+      <li>j</li>
+      <li>d</li>
+      <li>å</li>
+      </ul>
+      <ul className="grid">
+      <li>h</li>
+      <li>e</li>
+      <li>j</li>
+      <li>d</li>
+      <li>å</li>
+      </ul>
+      <ul className="grid">
+      <li>h</li>
+      <li>e</li>
+      <li>j</li>
+      <li>d</li>
+      <li>å</li>
+      </ul>
+    </section>
   )
 }
 
