da45c7aa044118040014067d254564743322bae0
alexDanilMain <alexdanilwork@gmail.com>
2024-03-28 14:19:44 +0100

    adds wordle logic

--- KindCoders-Wordle/src/App.tsx
+++ KindCoders-Wordle/src/App.tsx
@@ -1,50 +1,96 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import {promises as fs} from "fs";
+
+async function wordFetcher(): Promise<string>{
+  const filepath = "KindCoders-Wordle/src/data/Words.txt"
+  try {
+    const data = await fs.readFile(filepath, "utf-8")
+    const lines = data.split("/n").filter(Boolean)
+    if (lines.length === 0) {
+      throw new Error('The file is empty or does not exist.');
+    }
+    const randomIndex = Math.floor(Math.random() * lines.length);
+    return lines[randomIndex];
+  } catch (error) {
+    throw new Error(`Failed to read the file: ${error}`);
+  }
+  }
 
 function App() {
 
   const inputRef = useRef<HTMLInputElement>(null)
+  const ulref = useRef<HTMLUListElement>(null)
+
+
   const [guess, setGuess] = useState<string[]>([])
 
-  const goalWord = "spook";
+  const goalWord = "Spook";
 
-  const letterGuesser = () => {
+  const filterWord = () => {
+    let temp = goalWord
 
-    for(let i = 0; i < 5; i++){
-
-      if(guess[i] == goalWord[i]){
-        //Printa guess i som grön 
+    for (let i = 0; i < 5; i++) {
+      const listElement = ulref.current?.childNodes[i] as HTMLLIElement;
+      if (guess[i] == goalWord[i]) {
+        listElement.classList.add("bg-green-500" ,"text-black")
+        listElement.innerHTML = guess[i]
+        temp = temp.slice(0, i) + " " + temp.slice(i + 1, temp.length);
       }
-      else if(goalWord.includes(guess[i])){
-        //Printa guess i som gul
-      }
-      else{
-        //Printa som grå
-      }
-
     }
+
+    for (let i = 0; i < 5; i++) {
+      const listElement = ulref.current?.childNodes[i] as HTMLLIElement;
+
+      if (temp.includes(guess[i])) {
+        listElement.classList.add("bg-yellow-500", "text-black")
+        listElement.innerHTML = guess[i]
+        var index = temp.indexOf(guess[i]);
+        temp = temp.slice(0, index) + " "+ temp.slice(index + 1, temp.length);
+      }
+    }
+
+    for (let i = 0; i < 5; i++) {
+      const listElement = ulref.current?.childNodes[i] as HTMLLIElement;
+
+      if (listElement.innerHTML == "") {
+        listElement.classList.add("bg-gray-500", "text-white")
+        listElement.innerHTML = guess[i]
+      }
+    }
+
   }
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    setGuess(inputRef.current!.value.split(""))
-    
-
+    setGuess(prevGuess => [...prevGuess, ...inputRef.current!.value.toLowerCase().split("")]);
     inputRef.current!.value = "";
   }
 
+  useEffect(() => {
+    if(guess.length != 0){
+      filterWord(); 
+    }
+  }, [guess]);
+
   return (
-    
+
     <section className="container">
-      
+
       <h1>Hello World</h1>
-      
-      <form action="" onSubmit={(e)=>handleSubmit(e)}>
+
+      <form action="" onSubmit={(e) => handleSubmit(e)}>
         <input ref={inputRef} type="text" name="word" id="" minLength={5} maxLength={5} />
       </form>
 
-      <ul className="grid gap-x-4">
-      {guess.map(letter => <li className="h-12 w-12 border border-white rounded-sm list-none flex justify-center items-center">{letter}</li> )}
+      <ul className="grid gap-x-4 w-64 mx-auto" ref={ulref} >
+
+        <li id="list-0" className={`h-12 w-12 border border-white rounded-sm list-none flex justify-center items-center`}></li>
+        <li id="list-1" className={`h-12 w-12 border border-white rounded-sm list-none flex justify-center items-center`}></li>
+        <li id="list-2" className={`h-12 w-12 border border-white rounded-sm list-none flex justify-center items-center`}></li>
+        <li id="list-3" className={`h-12 w-12 border border-white rounded-sm list-none flex justify-center items-center`}></li>
+        <li id="list-4" className={`h-12 w-12 border border-white rounded-sm list-none flex justify-center items-center`}></li>
+
       </ul>
 
     </section>
