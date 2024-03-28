12fbeefc7746e030d31b844dfb0dfac311d940e6
Ewy Rostam <ewy.rost@gmail.com>
2024-03-28 11:40:24 +0100

    Add scetch for point system

--- KindCoders-Wordle/src/App.tsx
+++ KindCoders-Wordle/src/App.tsx
@@ -5,6 +5,25 @@ function App() {
   const inputRef = useRef<HTMLInputElement>(null)
   const [guess, setGuess] = useState<string[]>([])
 
+  const goalWord = "spook";
+
+  const letterGuesser = () => {
+
+    for(let i = 0; i < 5; i++){
+
+      if(guess[i] == goalWord[i]){
+        //Printa guess i som grön 
+      }
+      else if(goalWord.includes(guess[i])){
+        //Printa guess i som gul
+      }
+      else{
+        //Printa som grå
+      }
+
+    }
+  }
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
     e.preventDefault();
 
